5348c97687548e83b150a7ecf107d5e52cfa4092
Marko S <msalmikangas@gmail.com>
2021-07-07 13:45:03 +0300

    Markon kommentit

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -34,18 +34,18 @@ namespace DigiTrafficTester
             {
                 string lähtöasema;
                 string kohdeasema;
-                if (args.Length < 3)
-                {
-                    PrintUsage();
-                    return;
-                }
+                //if (args.Length < 3)
+                //{
+                //    PrintUsage();
+                //    return;
+                //}
                 lähtöasema = args[1];
                 kohdeasema = args[2];
                 TulostaJunatVälillä(lähtöasema, kohdeasema);
             }
             if (args[0].ToLower().StartsWith("-lj"))
             {
-                TulostaLiikkuvatJunat();
+                TulostaLiikkuvatJunat(asemat);
                 return;
             }
             if (args[0].ToLower().StartsWith("-e"))
@@ -66,7 +66,11 @@ namespace DigiTrafficTester
                 return;
             }
         }
-
+        /// <summary>
+        /// Muokattu, MS
+        /// </summary>
+        /// <param name="lähtöasema">lähtöaseman nimi</param>
+        /// <param name="kohdeasema">kohdeaseman nimi</param>
         private static void TulostaJunatVälillä(string lähtöasema, string kohdeasema)
         {
             RataDigiTraffic.APIUtil rata = new RataDigiTraffic.APIUtil();
@@ -74,7 +78,10 @@ namespace DigiTrafficTester
             string s = string.Join(", ", junat.Select(j => $"{j.trainNumber} {j.trainType}"));
             Console.WriteLine($"Junat {lähtöasema} ==> {kohdeasema}: " + s);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="asema">Vaihtoehtoinen parametri, palauttaa kaikki asemat tai "asema"lla alkavat asemat</param>
         private static void TulostaAsemat(string asema)
         {
             List<Liikennepaikka> paikat;
@@ -88,6 +95,10 @@ namespace DigiTrafficTester
                 }
             }
         }
+        /// <summary>
+        /// Haetaan rautatieverkoston asemat ja tehdään niistä sanakirja
+        /// </summary>
+        /// <returns></returns>
         private static Dictionary<string, string> HaeAsemat()
         {
 
@@ -103,21 +114,26 @@ namespace DigiTrafficTester
         /// <summary>
         /// Tulostaa kaikki liikenteessä olevat junat
         /// </summary>
-        private static void TulostaLiikkuvatJunat()
+        private static void TulostaLiikkuvatJunat(Dictionary<string,string> asemat)
         {
             RataDigiTraffic.APIUtil rata = new RataDigiTraffic.APIUtil();
             List<Juna> junat = rata.LiikkuvatJunat();
             foreach (var juna in junat)
             {
-                Console.WriteLine($"{juna.trainType,5} {juna.trainNumber,5}, Lähtöasema: {juna.timeTableRows[0].stationShortCode,-5} {juna.departureDate.ToShortDateString()} " +
-                    $" Määränpää: {juna.timeTableRows[^1].stationShortCode,-5} (lähtöaika: {juna.timeTableRows[0].actualTime.ToLocalTime().ToShortTimeString(),5} arvioitu saapumisaika: {juna.timeTableRows[^1].liveEstimateTime.ToLocalTime().ToShortTimeString(),5})");
+                string lahtoAsema;
+                string maaraAsema;
+                lahtoAsema = asemat[juna.timeTableRows[0].stationShortCode].Split(" ")[0];
+                maaraAsema = asemat[juna.timeTableRows[^1].stationShortCode].Split(" ")[0];
+
+                Console.WriteLine($"{juna.trainType}{juna.trainNumber}, Lähtöasema: {asemat[juna.timeTableRows[0].stationShortCode].Split(" ")[0]} {juna.departureDate.ToShortDateString()} " +
+                    $" Määränpää: {asemat[juna.timeTableRows[^1].stationShortCode].Split(" ")[0]} (lähtöaika: {juna.timeTableRows[0].actualTime.ToLocalTime().ToShortTimeString(),5} arvioitu saapumisaika: {juna.timeTableRows[^1].liveEstimateTime.ToLocalTime().ToShortTimeString(),5})");
             }
         }
         /// <summary>
         /// Tulosta haluttu juna junan tyypin/nimen (esim. IC) ja junan numeron mukaan. Jos junaa ei löydy, palauttaa "Etsittyä junaa ei löytynyt".
         /// </summary>
-        /// <param name="nimi"></param>
-        /// <param name="numero"></param>
+        /// <param name="nimi">junan nimi</param>
+        /// <param name="numero">junan numero</param>
         private static void TulostaEtsittyJuna(string nimi, int numero, Dictionary<string, string> stations)
         {
             RataDigiTraffic.APIUtil rata = new RataDigiTraffic.APIUtil();
@@ -195,10 +211,10 @@ namespace DigiTrafficTester
         /// <summary>
         /// Koordinaatiston etäisyyksien laskeminen.
         /// </summary>
-        /// <param name="a_lat"> aseman latitude </param>
-        /// <param name="a_long"> aseman longitude </param>
-        /// <param name="latitude"> kohteen x latitude </param>
-        /// <param name="longitude"> kohteen y longitude </param>
+        /// <param name="a_lat">aseman latitude </param>
+        /// <param name="a_long">aseman longitude </param>
+        /// <param name="latitude">kohteen x latitude </param>
+        /// <param name="longitude">kohteen y longitude </param>
         /// <returns> Palauttaa aseman ja kohteen x koordinaattien välisen etäisyyden. </returns>
         public static double PisteidenEtaisyys(double a_lat, double a_long, double latitude, double longitude)
         {
@@ -208,17 +224,18 @@ namespace DigiTrafficTester
             double res = Math.Sqrt(y_ero + x_ero);
             return res;
         }
-        private static void PrintUsage()
-        {
-            Console.WriteLine();
-            Console.WriteLine("Ohje:");
-            Console.WriteLine("-a[semat] <asemanAlkukirjain>");
-            Console.WriteLine("-j[unat] alkuasemaLyhenne loppuasemaLyhenne");
-            Console.WriteLine("tai");
-            Console.WriteLine("-e[tsi] junanTyyppi(IC) junanNumero ");
 
-            Console.WriteLine();
-        }
+        //private static void PrintUsage()
+        //{
+        //    Console.WriteLine();
+        //    Console.WriteLine("Ohje:");
+        //    Console.WriteLine("-a[semat] <asemanAlkukirjain>");
+        //    Console.WriteLine("-j[unat] alkuasemaLyhenne loppuasemaLyhenne");
+        //    Console.WriteLine("tai");
+        //    Console.WriteLine("-e[tsi] junanTyyppi(IC) junanNumero ");
+
+        //    Console.WriteLine();
+        //}
     }
 }
 
